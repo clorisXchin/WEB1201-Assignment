6ac2c39aeff1a24b88ea8158984ec9d7e4b742fa
ash-alaya-ai <infinitykaash3@gmail.com>
2026-08-07 19:52:55 +0800

    amen

--- js/portfolio_ash.js
+++ js/portfolio_ash.js
@@ -3,7 +3,7 @@ var cvMessage = document.getElementById("cvMessage");
 
 function downloadCV() {
     cvMessage.textContent =
-        "This is a simulated download — in a real deployment, this button would download Ash's CV as a PDF.";
+        "This is only a simulated download";
 }
 
 if (downloadCvBtn !== null) {
